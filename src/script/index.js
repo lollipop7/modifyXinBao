419636b1df9d4b5ac7c87a3a0c738cc94bd77b64
lollipop7 <juanlilu@yeah.net>
2018-09-03 18:17:53 +0800

    首页

---
+++ src/script/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by lollipop on 2018/9/3
+ */
+layui.use(['jquery', 'carousel'], function(){
+    const $ = layui.jquery,
+        carousel = layui.carousel;
+    carousel.render({
+        elem: '#banner-carousel'
+        ,height: '640px'
+        ,width: '100%' //设置容器宽度
+        ,arrow: 'none' //始终显示箭头
+        //,anim: 'updown' //切换动画方式
+    });
+
+});
